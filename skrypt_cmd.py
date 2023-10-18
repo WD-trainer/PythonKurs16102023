ad97a38f27bb83d08571bb923a178aab68bbd6ef
WD-trainer <wojtekdudzik@o2.pl>
2023-10-18 12:39:53 +0200

    Modul click

---
+++ skrypt_cmd.py
@@ -0,0 +1,42 @@
+# import click
+#
+# @click.command()
+# @click.option("--count", default=1, help="Number of greetings.", required=True)
+# @click.option("--name", prompt="Your name", help="The person to greet.")
+# @click.option('--password',prompt=True, confirmation_prompt=True, hide_input=True)
+# def hello(count, name, password):
+#     """Simple program that greets NAME for a total of COUNT times."""
+#     for _ in range(count):
+#         click.echo(f"Hello, {name}!")
+#         click.echo(f'Moje super tajne hasło: {password}')
+#
+# if __name__ == '__main__':
+#     hello()
+
+import os
+
+def znajdz_pliki_i_katalogi(katalog_startowy, fraza):
+    """
+    Ta funckja zajmuje sie znajdowaniem katalowgo i plikow. Ignoruje wielkosci liter
+    Parameters
+    ----------
+    katalog_startowy
+    fraza
+
+    Returns
+    -------
+
+    """
+    znalezione_elementy = []
+
+    fraza = fraza.lower()
+
+    for root, dirs, files in os.walk(katalog_startowy):
+        for element in dirs + files:
+            if fraza in element.lower():
+                znalezione_elementy.append(os.path.join(root, element))
+
+    print(znalezione_elementy)
+
+
+# opkauj powyzsza funckje przy uzyciu bibloteki click
